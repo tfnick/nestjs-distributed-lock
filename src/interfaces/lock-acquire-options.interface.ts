d7693c39cfe574e6ff07b970451d7d41face917b
tfnick <tfnick.liu@gmail.com>
2025-12-01 14:33:11 +0800

    fix bug

--- src/interfaces/lock-acquire-options.interface.ts
+++ src/interfaces/lock-acquire-options.interface.ts
@@ -1,9 +1,6 @@
 export interface LockAcquireOptions {
   /** 锁超时时间（毫秒） */
   timeout?: number;
-export interface LockAcquireOptions {
-  /** 锁超时时间（毫秒） */
-  timeout?: number;
   
   /** 是否等待获取锁（true：等待直到超时，false：立即返回） */
   wait?: boolean;
